92f6a2b18ff578a69f4902a8b2e963cc83f5338d
Carlgo11 <carlgo11@carlgo11.com>
2014-11-14 15:04:20 +0100

    fixes n stuff

---
+++ resources/footer.php
@@ -0,0 +1,5 @@
+<div id="footer_wrapper" >
+    <div id="footer">
+        Copyright © <?php echo date("Y"); ?> <a href="./">HtsIT</a>
+    </div>
+</div>
---
+++ resources/navbar.php
@@ -0,0 +1,20 @@
+<div id="header"> 
+    <div id="logo"><h1><a href="http://www.htsIT.se" target="_parent">Hts</a></h1></div>
+
+    <div id="meny" class="meny">
+        <ul>
+            <li><a href="http://www.htsIT.se" class="selected">Hem</a></li>
+            <li><a href="Om.html">Om</a>
+
+            </li>
+            <li><a href="kurser.html">Kurser</a>
+            </li>
+            <li><a href="foton.html">Foton</a>
+
+            </li>
+            <li><a href="http://htsit.se/htk/" target="_blank">HTK</a></li>
+        </ul>
+        <br style="clear: left" />
+    </div>
+
+</div>
