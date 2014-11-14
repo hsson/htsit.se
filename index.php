1ebdc49c7d726ba7e6da8eb12d7b2e104e0aeba0
Carlgo11 <carlgo11@carlgo11.com>
2014-11-14 15:13:16 +0100

    moved index.html > .php & merged index.php with indexnjs.php

--- index.php
+++ index.php
@@ -2,18 +2,14 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <!-- Dokumentets titel -->
         <title>HtsIT</title>
-        <!-- Länkar till stilmallen "stilen.css" -->
-        <link href="stilen.css" type="text/css" rel="stylesheet" />
-        <!-- Länkar till stilmallen "meny.css" -->
-        <link rel="stylesheet" type="text/css" href="meny.css" />
-        <!-- Javascript -->
-        <script type="text/javascript" src="scripts/swfobject/swfobject.js"></script>
+        <link href="htsit/stilen.css" type="text/css" rel="stylesheet" />
+        <link rel="stylesheet" type="text/css" href="htsit/meny.css" />
+        <script type="text/javascript" src="htsit/piecemaker/scripts/swfobject/swfobject.js"></script>
         <script type="text/javascript">
             var flashvars = {};
-            flashvars.cssSource = "css/piecemaker.css";
-            flashvars.xmlSource = "piecemaker.xml";
+            flashvars.cssSource = "htsit/piecemaker/css/piecemaker.css";
+            flashvars.xmlSource = "htsit/piecemaker/piecemaker.xml";
 
             var params = {};
             params.play = "true";
@@ -24,30 +20,31 @@
             params.allowscriptaccess = "always";
             params.allownetworking = "all";
 
-            swfobject.embedSWF('piecemaker.swf', 'piecemaker', '960', '500', '10', null, flashvars,
+            swfobject.embedSWF('htsit/piecemaker/piecemaker.swf', 'piecemaker', '960', '500', '10', null, flashvars,
                     params, null);
 
         </script>
 
     </head>
 </body>
-
 <div id="wrapper">
-    <?php include './resources/navbar.php'; ?>
-    <div id="Meddelande">     
+
+    <?php include './resources/navbar.php';?>
+    <div id="Meddelande">
         <marquee onMouseover="this.scrollAmount = 1" onMouseout="this.scrollAmount = 6">
             <font color="#86d2ff">[HtsIT]</font> Välkommen till HTSIT! Twitter: <a href="https://twitter.com/htsit3040">@htsit3040</a>
         </marquee>
         <div id="slut"></div>
-        <div id="start"></div>     
+        <div id="start"></div>
+    </div>
+    <div id="piecemaker_wh">
+        <div id="piecemaker">
+
+        </div>
     </div>
 
-    </br></br>
-    <img src="images/gallery/HTS.jpg" alt="image 2" />
-
-
     <div class="main_wh">
-    </div>	
+    </div>
     <div id="links">
         <div class="main3_wh">
 
@@ -57,16 +54,16 @@
             <h5><a href="https://fronter.com/hassleholm/" class="more">Fronter</a></h5>
             <h5><a href="https://sms8.schoolsoft.se/hassleholm/jsp/Login.jsp" class="more">Schoolsoft</a></h5>
             <h5><a href="http://www.hts.se/mat/" class="more">Matsedel</a></h5>
-            <h5><a href="http://www.htsIT.se" class="more">Yes Javascript</a></h5></br></br></br></br>
+            <h5><a href="htsit/indexnjs.html" class="more">No Javascript</a></h5><br /><br /><br /><br />
             <h2>Kom ihåg:</h2>
-            <p><b>Gymnasiearbetet ska vara inlämnat denna veckan, onsdagen den 30 april.</br></br>
-                    Muntligt nationella prov i svenska tisdagen den 13 maj.</br></br>
-                    Slutprov i kemi fredagen den 16 maj.</br></br></br></br></br></b></p>
+            <p><b>Gymnasiearbetet ska vara inlämnat denna veckan, onsdagen den 30 april.<br /><br />
+                    Muntligt nationella prov i svenska tisdagen den 13 maj.<br /><br />
+                    Slutprov i kemi fredagen den 16 maj.<br /><br /><br /><br /><br /><br /></b></p>
             <h2>Kontakt:</h2>
-            <p><b> Har du funderingar om hemsidan? </br>Eller några förslag om förbättringar?</br> Hör av er till mig på min email:</br>Ludwig.wolff@hotmail.com</br></br></br></br></b></p>
+            <p><b> Har du funderingar om hemsidan? <br />Eller några förslag om förbättringar?<br /> Hör av er till mig på min email:<br />Ludwig.wolff@hotmail.com<br /><br /><br /><br /><br /></b></p>
 
-        </div>	
-    </div>	
+        </div>
+    </div>
 
     <div id="main">
         <div class="main2_wh">
@@ -91,8 +88,7 @@
             <p>Det här är en inofficiell sida för IT-inriktningen på HTS Teknikprogram. Vill du veta mer om HTS ska du gå till skolans hemsida <a href="http://www.hts.se">http://www.hts.se</a>.</p>	
         </div>	
     </div>
-    <?php include'./resources/footer.php'; ?>
-
-</div>	
+    <?php include'./resources/footer.php';?>
+</div>
 </body>
 </html>
