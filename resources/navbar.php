291b760343817fbf7f5e27be6572f6101e0291d6
Carlgo11 <carlgo11@carlgo11.com>
2014-11-14 15:08:58 +0100

    foto.html b-4gone

--- resources/navbar.php
+++ resources/navbar.php
@@ -3,14 +3,11 @@
 
     <div id="meny" class="meny">
         <ul>
-            <li><a href="http://www.htsIT.se" class="selected">Hem</a></li>
-            <li><a href="Om.html">Om</a>
+            <li><a href="./" class="selected">Hem</a></li>
+            <li><a href="Om.php">Om</a>
 
             </li>
             <li><a href="kurser.html">Kurser</a>
-            </li>
-            <li><a href="foton.html">Foton</a>
-
             </li>
             <li><a href="http://htsit.se/htk/" target="_blank">HTK</a></li>
         </ul>
